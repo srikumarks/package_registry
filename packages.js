ebb4f0accd43547b0897a3097f9e58dcdf195186
Srikumar K. S <srikumarks@gmail.com>
2012-05-20 23:31:25 +0800

    added popular libraries

---
+++ packages.js
@@ -0,0 +1,33 @@
+// popular libraries.
+package.config({
+    'jQuery': { external: { name: '$'
+        , url: 'http://code.jquery.com/jquery-1.7.2.js'
+        , dependsOn: []
+        , depNames: []
+    }},
+
+    'Underscore': { external: { name: '_'
+        , url: 'https://raw.github.com/documentcloud/underscore/master/underscore.js'
+        , dependsOn: []
+        , depNames: []
+    }},
+
+    'Backbone': { external: { name: 'Backbone'
+        , url: 'https://raw.github.com/documentcloud/backbone/master/backbone.js'
+        , dependsOn: ['Underscore']
+        , depNames: ['_']
+    }},
+
+    'Prototype': { external: { name: 'Prototype'
+        , url: 'https://ajax.googleapis.com/ajax/libs/prototype/1.7.0.0/prototype.js'
+        , dependsOn: []
+        , depNames: []
+    }},
+
+    'MooTools': { external: { name: 'MooTools'
+        , url: 'http://mootools.net/download/get/mootools-core-1.4.5-full-nocompat.js'
+        , dependsOn: []
+        , depNames: []
+    }}
+});
+
